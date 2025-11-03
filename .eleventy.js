a679b3707084b8b11e1be3aaad997db02312fe81
banishga-cell <banishga@msu.edu>
2025-11-03 01:51:28 +0000

    finish

--- .eleventy.js
+++ .eleventy.js
@@ -3,4 +3,13 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+  eleventyConfig.addLiquidShortcode("itemCard", function(name, image, description) {
+    return `
+      <div class="item-card">
+        <img src="${image}" alt="${name}" class="item-image">
+        <h2>${name}</h2>
+        <p>${description}</p>
+      </div>
+    `;
+  });
 };
--- about.md
+++ about.md
@@ -4,4 +4,5 @@ title: About
 ---
 
 # About the 11ty **Boilerplate**
+<p>This is a test page for the 11ty boilerplate. It'll turn into an about page in the future</p>
 <img class="about" alt="pine" src="/images/pine.png" width="50" />
